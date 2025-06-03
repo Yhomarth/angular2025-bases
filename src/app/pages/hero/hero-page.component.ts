ca4309c574f5dde608a0324c9b409e382387c05c
Yhomarth Reyes <y.reyes@simv.gob.do>
2025-06-03 16:25:24 -0400

    tarea completada

---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+  templateUrl: './hero-page.component.html'
+})
+export class HeroPageComponent {
+
+  name = signal('Ironman');
+  age = signal(45);
+
+
+  getHeroDescription(){
+    return `${this.name() } - ${this.age()}`;
+  }
+
+  changeHero(){
+    this.name.set('Spiderman');
+  }
+
+  changeAge(){
+    this.age.update(currentAge => currentAge + 5);
+  }
+
+ resetForm(){
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+
+}
